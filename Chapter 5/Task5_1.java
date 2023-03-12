242b8c9c123a160d3ba8d29125074c26c6f3ff4b
KrasovskiiNN <KrasovskiiNN@gmail.com>
2023-03-12 21:59:59 +0500

    Chapter5

--- Chapter 5/Task5_1.java
+++ Chapter 5/Task5_1.java
@@ -1,2 +1,11 @@
-public class Task5_1 {
-}
+public class Task5_1
+    {
+        public static void main(String[] args)
+            {
+                int a=0;
+                int b=-3;
+                int c=9;
+
+
+            }
+    }
