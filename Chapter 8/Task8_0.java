7c546ff129f461fc8b47a6d015ecc71e4ebdca03
KrasovskiiNN <KrasovskiiNN@gmail.com>
2023-04-14 22:44:59 +0500

    Chapter8

---
+++ Chapter 8/Task8_0.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Task8_0
+    {
+        public static Scanner reader = new Scanner(System.in);
+
+        public static void main(String[] args)
+            {
+                int a = reader.nextInt();
+
+                if (a>5) System.out.println("OK 555");
+
+                System.out.println("qwerty");
+
+                reader.close();
+            }
+    }
